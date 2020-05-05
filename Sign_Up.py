9e9a08cc434cad3389f9b1e1e8c161ec5b0a54d9
VitaliyBobyk <falcone159357bv@gmail.com>
2020-05-05 13:03:06 +0300

    aded simple GUI interface

--- Sign_Up.py
+++ Sign_Up.py
@@ -1,16 +1,31 @@
 from email_validator import validate_email, EmailNotValidError
 from password_strength import PasswordPolicy
+from tkinter import messagebox as mb
+from tkinter import *
+
+root = Tk()
+
+root.resizable(width=False, height=False)
+root.geometry('300x300')
+root.title('Sign Up')
+root['bg'] = 'black'
+ei = Entry(root, width=40)
+el = Label(root, text="Enter your e-mail", bg="black",fg='white', width=20)
+pl = Label(root, text="Enter your password", bg="black",fg='white', width=20)
+pi = Entry(root, width=40)
+cp = Label(root, text="Confirm your password", bg="black",fg='white', width=20)
+ci = Entry(root, width=40)
+b = Button(root, text="Sign Up!")
 
 def check_mail():
-    email = str(input('Enter your e-mail: '))
+    email = ei.get()
     try:
         valid = validate_email(email)
         email = valid.email
         return email
     except EmailNotValidError as e:
-        # email is not valid, exception message is human-readable
-        print(str(e))
-        print(email)
+        mb.showerror('Erorr in e-mail','The email address is not valid. It must have exactly one @-sign.')
+        return None
 def check_p():
     policy = PasswordPolicy.from_names(
         length=8,  # min length: 8
@@ -18,23 +33,15 @@ def check_p():
         numbers=2,  # need min. 2 digits
         nonletters=2,  # need min. 2 non-letter characters (digits, specials, anything)
     )
-    password = str(input('Enter your password: '))
-    while policy.test(password) != []:
-        print(policy.test(password))
-        password = str(input('Enter your password: '))
-    if policy.test(password) == []:
-        confirm_p = str(input('Confirm your password: '))
-    if password == confirm_p:
-        print('Well done, you have made the registration')
+    password = pi.get()
+    confirm_p = ci.get()
+    tp = policy.test(password)
+    tc = policy.test(confirm_p)
+    if tp == [] and tc == []:
+        mb.showinfo('Congratulations!','Well done, you have made the registration')
         return password
-    while password != confirm_p:
-        print("Password's not the same")
-        confirm_p = str(input('Confirm your password: '))
-        if password == confirm_p:
-            print('Well done, you have made the registration')
-            return password
-        return password
-    return password
+    else:
+        return mb.showerror("Erorr password", tc)
 
 def incl():
     vm = check_mail()
@@ -43,12 +50,22 @@ def incl():
         total = vm + " " + vp
         return total
 
-
 def write_to_db():
     f = open('database.txt', 'a')
     par_func = incl()
     if par_func != None:
         f.write(par_func + '\n')
         f.close
+        return root.destroy()
 
-write_to_db()
+def leftclick(event):
+    write_to_db()
+el.pack()
+ei.pack()
+pl.pack()
+pi.pack()
+cp.pack()
+ci.pack()
+b.pack()
+b.bind('<Button-1>', leftclick)
+root.mainloop()
--- log.py
+++ log.py
@@ -1,18 +1,40 @@
+from tkinter import messagebox as mb
+from tkinter import *
 f = open('database.txt')
 s = f.read()
 list_db = s.split()
 
-log_in = str(input('Enter your e-mail: '))
-password = str(input('Enter your password: '))
+root = Tk()
+root.resizable(width=False, height=False)
+root.geometry('300x300')
+root.title('Sign Up')
+root['bg'] = 'black'
+ei = Entry(root, width=40)
+el = Label(root, text="Enter your e-mail", bg="black",fg='white', width=20)
+pl = Label(root, text="Enter your password", bg="black",fg='white', width=20)
+pi = Entry(root, width=40)
+b = Button(root, text="Log In!!")
 
-for i in list_db:
-    if i == log_in:
-        ind_l = list_db.index(i)
-        if list_db[ind_l + 1] == password:
-            print('You have successfully logged in!')
-        else:
-            print('Login or password incorrect')
+def finish():
+    log_in = ei.get()
+    password = pi.get()
+    ind_l = 0
+    for i in list_db:
+        if i == log_in:
+            ind_l = list_db.index(i)
+        if i == log_in  == log_in and list_db[ind_l + 1] == password:
+            f.close()
+            mb.showinfo('Congratulations!','You have successfully logged in!')
+            return root.destroy()
+    f.close()
+    mb.showerror('Eroor!!','Login or password incorrect!')
 
-
-
-f.close()
+def leftclick(event):
+    finish()
+el.pack()
+ei.pack()
+pl.pack()
+pi.pack()
+b.pack()
+b.bind('<Button-1>', leftclick)
+root.mainloop()
